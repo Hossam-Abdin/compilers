c2295033910083c32e6dbbb159b39138c36a0d5f
Hossam-Abdin <hossamabdeen17@gmail.com>
2020-12-07 17:25:28 +0100

    goto versions

---
+++ endterm_prep/code_gen/goto_1/example.asm
@@ -0,0 +1,82 @@
+global main
+extern read_unsigned, write_unsigned
+extern read_boolean, write_boolean
+segment .bss
+label0: resd 1
+label1: resd 1
+label2: resd 1
+label3: resb 1
+
+segment .text
+main:
+call read_unsigned
+mov [label0], eax
+mov al, 0
+mov [label3], al
+mov eax, 2
+mov [label1], eax
+label4:
+label6:
+mov eax, [label0]
+push eax
+mov eax, [label1]
+pop ebx
+cmp eax, ebx
+setl al
+push ax
+mov al, [label3]
+xor al, 1
+pop bx
+and al, bl
+cmp al, 1
+jne near label7
+mov eax, 0
+push eax
+xor edx, edx
+mov eax, [label1]
+push eax
+mov eax, [label0]
+pop ebx
+div ebx
+mov eax, edx
+pop ebx
+cmp eax, ebx
+sete al
+cmp al, 1
+jne near label5
+mov al, 1
+mov [label3], al
+mov eax, [label1]
+mov [label2], eax
+label5:
+mov eax, 1
+push eax
+mov eax, [label1]
+pop ebx
+add eax, ebx
+mov [label1], eax
+jmp label4
+jmp label6
+label7:
+mov al, [label3]
+cmp al, 1
+jne near label8
+xor eax, eax
+mov al, [label3]
+push eax
+call write_boolean
+add esp,4
+mov eax, [label2]
+push eax
+call write_unsigned
+add esp,4
+jmp label9
+label8:
+xor eax, eax
+mov al, [label3]
+push eax
+call write_boolean
+add esp,4
+label9:
+
+ret
---
+++ endterm_prep/code_gen/goto_1/semantics.h
@@ -0,0 +1,33 @@
+#ifndef SEMANTICS_H
+#define SEMANTICS_H
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<sstream>
+
+enum type { integer, boolean, goto_label };
+
+struct expression_descriptor {
+	type expr_type;
+	std::string expr_code;
+	expression_descriptor(type t, std::string s) {
+		expr_type = t;
+		expr_code = s;
+	}
+};
+
+struct var_data {
+	int decl_row;
+	type decl_type;
+    std::string label;
+	var_data(){}
+	var_data(int i, type t, std::string l)
+	{
+		decl_row = i;
+		decl_type = t;
+        label = l;
+	}
+};
+
+#endif
